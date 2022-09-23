a8d278dd1b28668fd647ecd37bddb1ebeb36c118
HOANGANH\Lenovo <hoanganh5111999@gmail.com>
2022-09-23 15:59:13 +0700

    cách khởi tạo slice trong Go.

--- slice.go
+++ slice.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//khai báo 1 slice
+	var slice []int
+	fmt.Println(slice)
+	//Khai bao va khoi tao slice
+	var slice1 = []int{1, 2, 3, 4}
+	fmt.Println(slice1)
+	//tao mot slice tu mot array
+	var array = [4]int{1, 2, 3, 4} //array[1]=2 array[2]=3
+	slice2 := array[1:3]           //array[1] -> array[3-1] <-> array[2]
+	fmt.Println(slice2)
+
+	slice3 := array[:] //all
+	fmt.Println(slice3)
+
+	slice4 := array[2:] //2,3
+	fmt.Println(slice4)
+
+	slice5 := array[:3] // 0,1,2
+	fmt.Println(slice5)
 }
